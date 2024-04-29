690975e31227c9eccf35ea31880ef5443954d4a7
JuniorLima04 <128000358+JuniorLima04@users.noreply.github.com>
2024-04-29 20:27:45 -0300

    Add files via upload

---
+++ The Rick and Morty ValdirJr_Aluno/src/components/ListCharacters.vue
@@ -0,0 +1,20 @@
+<script setup>
+const props = defineProps(["character"])
+</script>
+
+<template>
+  <div class="col-md-4">
+    <div class="card p-3 mb-4">
+      <img :src="character.image" height="auto" alt="" class="rounded-circle mx-auto d-block">
+      <div class="card-body text-center">
+        <p class="mb-1"><strong>Nome:</strong> {{ character.name }}</p>
+        <p class="mb-1"><strong>Status:</strong> {{ character.status }}</p>
+        <p class="mb-1"><strong>Espécie:</strong> {{ character.species }}</p>
+        <p class="mb-1"><strong>Gênero:</strong> {{ character.gender }}</p>
+        <p class="mb-1"><strong>Localização:</strong> {{ character.location.name }}</p>
+        <p class="mb-0"><strong>Episódios:</strong> {{ character.episode.length }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
---
+++ The Rick and Morty ValdirJr_Aluno/src/views/AboutView.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="about">
+    Aluno: Valdir Junior Petrarca De lima <br>
+    RA: 1969935 <br>
+    Curso: Análise e Desenvolvimento de Sistemas<br>
+    Turma: 3C<br>
+  </div>
+</template>
